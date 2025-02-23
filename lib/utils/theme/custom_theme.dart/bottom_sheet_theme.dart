497dfb45e94a88f9c8e214b2a005a483bc06c32d
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-02-23 11:32:26 +0530

    created bottom sheet theme

---
+++ lib/utils/theme/custom_theme.dart/bottom_sheet_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class IBottomSheetTheme {
+  const IBottomSheetTheme._();
+
+  static BottomSheetThemeData lightBottomSheetTheme = BottomSheetThemeData(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(16)),
+    ),
+    showDragHandle: true,
+    backgroundColor: Colors.white,
+    modalBackgroundColor: Colors.white,
+    constraints: BoxConstraints(minWidth: double.infinity),
+  );
+
+  static BottomSheetThemeData darkBottomSheetTheme = BottomSheetThemeData(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(16)),
+    ),
+    showDragHandle: true,
+    backgroundColor: Colors.black,
+    modalBackgroundColor: Colors.black,
+    constraints: BoxConstraints(minWidth: double.infinity),
+  );
+}
