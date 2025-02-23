f5417795336c28b07c2c0e95a465609c41cda9d0
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-02-23 11:37:15 +0530

    created checkbox theme

---
+++ lib/utils/theme/custom_theme.dart/check_box_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ICheckBoxTheme {
+  const ICheckBoxTheme._();
+
+  static CheckboxThemeData lightCheckBoxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateColor.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      }
+      return Colors.black;
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      }
+      return Colors.transparent;
+    }),
+  );
+
+  static CheckboxThemeData darkCheckBoxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateColor.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      }
+      return Colors.black;
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      }
+      return Colors.transparent;
+    }),
+  );
+}
