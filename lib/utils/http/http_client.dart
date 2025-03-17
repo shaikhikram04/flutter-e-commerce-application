c03a22c7bff37c7d035b6330b3bb7c0d561df0bb
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-03-18 03:48:07 +0530

    created http client

---
+++ lib/utils/http/http_client.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class IHttpHelper {
+  static const String _baseUrl = 'https://your-api-base-url.com';
+
+  //* Helper method to make a GET request
+  static Future<Map<String, dynamic>> get(String endPoint) async {
+    final response = await http.get(Uri.parse('$_baseUrl/$endPoint'));
+    return _handleResponse(response);
+  }
+
+  //* Helper method to make a POST request
+  static Future<Map<String, dynamic>> post(
+    String endPoint,
+    dynamic data,
+  ) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/$endPoint'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(data),
+    );
+    return _handleResponse(response);
+  }
+
+  //* Helper method to make a PUT request
+  static Future<Map<String, dynamic>> put(String endPoint, dynamic data) async {
+    final response = await http.put(
+      Uri.parse('$_baseUrl/$endPoint'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(data),
+    );
+    return _handleResponse(response);
+  }
+
+  //* Helper method to make a DELETE request
+  static Future<Map<String, dynamic>> delete(
+    String endPoint,
+  ) async {
+    final response = await http.delete(Uri.parse('$_baseUrl/$endPoint'));
+    return _handleResponse(response);
+  }
+
+  //* handle the HTTP response
+  static Map<String, dynamic> _handleResponse(http.Response response) {
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load data : ${response.statusCode}');
+    }
+  }
+}
