7da1bee27393182c2c98c1b5cd81bfefb8ea4c24
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-03-20 03:42:14 +0530

    added on-boarding images url

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -6,5 +6,10 @@ class IImages {
   //* Social logos
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
+
+  //* Onboarding Images
+  static const String searching = "https://lottie.host/ae1c4187-4bbb-499c-a273-64c14641cc63/WNO4p0bBdW.lottie";
+  static const String shopping = "https://lottie.host/3a8af77a-266f-46a7-9621-38fc230c7ad4/8J5eNvVPuC.lottie";
+  static const String delivery = "https://lottie.host/3613b437-23ce-409c-949d-a5765fd0445a/tRCg2gjdc9.lottie";
   
 }
