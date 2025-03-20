e660c685a70bd4ce0be308234e58115a79a1f219
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-03-21 02:49:08 +0530

    created onBoarding screen pages

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/features/authentication/screens/onboarding_screen.dart';
 import 'package:flutter_e_commerce/utils/theme/theme.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: IAppTheme.lightTheme,
       darkTheme: IAppTheme.darkTheme,
+      home: OnboardingScreen(),
     );
   }
 }
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,15 +1,19 @@
 class IImages {
   //*  App Logos
-  static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
-  static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
+  static const String darkAppLogo =
+      "assets/logos/t-store-splash-logo-black.png";
+  static const String lightAppLogo =
+      "assets/logos/t-store-splash-logo-white.png";
 
   //* Social logos
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
 
   //* Onboarding Images
-  static const String searching = "https://lottie.host/ae1c4187-4bbb-499c-a273-64c14641cc63/WNO4p0bBdW.lottie";
-  static const String shopping = "https://lottie.host/3a8af77a-266f-46a7-9621-38fc230c7ad4/8J5eNvVPuC.lottie";
-  static const String delivery = "https://lottie.host/3613b437-23ce-409c-949d-a5765fd0445a/tRCg2gjdc9.lottie";
-  
+  static const String onBoardingImage1 =
+      "assets/animations/searching.json";
+  static const String onBoardingImage2 =
+      "assets/animations/shopping.json";
+  static const String onBoardingImage3 =
+      "assets/animations/delivery.json";
 }
