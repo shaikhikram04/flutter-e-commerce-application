a16a69d25e77d29f3fec7838d95bda0fbb25df9f
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-03-22 03:31:24 +0530

    added divider in login screen

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce/common/style/spacing_styles.dart';
+import 'package:flutter_e_commerce/utils/constants/colors.dart';
 import 'package:flutter_e_commerce/utils/constants/image_strings.dart';
 import 'package:flutter_e_commerce/utils/constants/sizes.dart';
 import 'package:flutter_e_commerce/utils/constants/text_strings.dart';
 import 'package:flutter_e_commerce/utils/helpers/helper_functions.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -106,11 +108,37 @@ class LoginScreen extends StatelessWidget {
                           child: const Text(ITexts.createAccount),
                         ),
                       ),
-                      const SizedBox(height: ISizes.spaceBtwSections),
                     ],
                   ),
                 ),
-              )
+              ),
+
+              //* Divider
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Divider(
+                      color: dark ? IColors.darkGrey : IColors.grey,
+                      thickness: 0.5,
+                      indent: 60,
+                      endIndent: 5,
+                    ),
+                  ),
+                  Text(
+                    ITexts.orSignInWith.capitalize!,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  Flexible(
+                    child: Divider(
+                      color: dark ? IColors.darkGrey : IColors.grey,
+                      thickness: 0.5,
+                      indent: 5,
+                      endIndent: 60,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
