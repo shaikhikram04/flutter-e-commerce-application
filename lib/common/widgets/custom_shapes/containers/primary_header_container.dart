280df0757a7bd6e1fbcfe7b891fa7e68ed4da1a7
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-04-26 20:29:54 +0530

    created header container with clipper

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/common/widgets/curved_edges/curved_edges_widget.dart';
+import 'package:flutter_e_commerce/common/widgets/custom_shapes/containers/circular_containers.dart';
+import 'package:flutter_e_commerce/utils/constants/colors.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        height: 350,
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: Stack(
+          children: [
+            Positioned(
+              top: -150,
+              right: -250,
+              child: TCircularContainer(
+                backgroundColor: TColors.textWhite.withValues(alpha: 0.1),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              right: -300,
+              child: TCircularContainer(
+                backgroundColor: TColors.textWhite.withValues(alpha: 0.1),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home.dart
+++ lib/features/shop/screens/home.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
