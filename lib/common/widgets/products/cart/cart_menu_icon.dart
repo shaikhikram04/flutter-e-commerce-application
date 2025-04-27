1535fe2e9e76f008d2e42a70097f363211ef1980
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-04-28 00:52:55 +0530

    Design Reusable Custom AppBar

---
+++ lib/common/widgets/products/cart/cart_menu_icon.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/utils/constants/colors.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TCartCounterIcon extends StatelessWidget {
+  const TCartCounterIcon({
+    super.key,
+    required this.onPressed,
+    required this.iconColor ,
+  });
+
+  final Color iconColor;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            Iconsax.shopping_bag,
+            color: iconColor,
+          ),
+        ),
+        Positioned(
+          right: 0,
+          child: Container(
+            width: 18,
+            height: 18,
+            decoration: BoxDecoration(
+              color: Colors.black54,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Center(
+                child: Text(
+              '2',
+              style: Theme.of(context).textTheme.labelLarge!.apply(
+                    color: TColors.white,
+                    fontSizeFactor: 0.8,
+                  ),
+            )),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/features/shop/screens/widgets/home_app_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/common/widgets/app_bar/app_bar.dart';
+import 'package:flutter_e_commerce/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:flutter_e_commerce/utils/constants/colors.dart';
+import 'package:flutter_e_commerce/utils/constants/text_strings.dart';
+
+class THomeAppBar extends StatelessWidget {
+  const THomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TAppBar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            TTexts.homeAppBarTitle,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .apply(color: TColors.grey),
+          ),
+          Text(
+            TTexts.homeAppBarSubTitle,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .apply(color: TColors.white),
+          ),
+        ],
+      ),
+      actions: [
+        TCartCounterIcon(
+          onPressed: () {},
+          iconColor: TColors.white,
+        ),
+      ],
+    );
+  }
+}
